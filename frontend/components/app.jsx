16f46965a693b61e16a2d4eb2d4785b40575f716
Allen Chen <alchen1231@gmail.com>
2017-05-18 15:19:56 -0700

    working footer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,7 +3,7 @@ import GreetingContainer from './greeting/greeting_container';
 import { Route } from 'react-router-dom';
 import ListingsIndexContainer from './listings/listings_index_container';
 import Home from './home/home';
-
+import Footer from './footer/footer';
 
 const App = () => (
   <div className='header'>
@@ -13,6 +13,7 @@ const App = () => (
       <Route exact path='/' component={Home}/>
       <Route path='/listings' component={ListingsIndexContainer}/>
     </div>
+    <Footer />
   </div>
 );
 
--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,40 +1,42 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 class ListingsIndex extends React.Component {
 
   render() {
-
-
     return (
-      <div>
-        <label>ABOUT</label>
-        <par>
+      <div className='footerLinks'>
+        <par className='footerLinksColumn'>
+        <label className='footerColumnCategory'>ABOUT</label>
           Paragraph about my project goes here
         </par>
 
+        <div className='footerLinksColumn'>
+          <label className='footerColumnCategory'>AirBadnBoujie</label>
+          <Link to="/about-us">About Us</Link>
+          <Link to="/careers">Careers</Link>
+          <Link to="/press">Press</Link>
+          <Link to="/policies">Policies</Link>
+          <Link to="/help">Help</Link>
+          <Link to="/diversity">Diversity & Belonging</Link>
+        </div>
 
-        <label>AirBadnBoujie</label>
-        <Link to="/about-us">About Us</Link>
-        <Link to="/careers">Careers</Link>
-        <Link to="/press">Press</Link>
-        <Link to="/policies">Policies</Link>
-        <Link to="/help">Help</Link>
-        <Link to="/diversity">Diversity & Belonging</Link>
+        <div className='footerLinksColumn'>
+          <label className='footerColumnCategory'>Discover</label>
+          <Link to="/trust">Trust & Safety</Link>
+          <Link to="/travel-credit">Travel Credit</Link>
+          <Link to="/giftcards">Gift Cards</Link>
+          <Link to="/business-travel">Business Travel</Link>
+          <Link to="/guidebooks">Guidebooks</Link>
+        </div>
 
-        <label>Discover</label>
-        <Link to="/trust">Trust & Safety</Link>
-        <Link to="/travel-credit">Travel Credit</Link>
-        <Link to="/giftcards">Gift Cards</Link>
-        <Link to="/business-travel">Business Travel</Link>
-        <Link to="/guidebooks">Guidebooks</Link>
-
-        <label>Social</label>
-        <Link to="/why-host">Why Host</Link>
-        <Link to="/hospitality">Hospitality</Link>
-        <Link to="/responsible-hosting">Listings</Link>
-        <Link to="/communication-center">Communication Center</Link>
-        <Link to="/listings">Listings</Link>
-
+        <div className='footerLinksColumn'>
+          <label className='footerColumnCategory'>Social</label>
+          <Link to="/why-host">Why Host</Link>
+          <Link to="/hospitality">Hospitality</Link>
+          <Link to="/responsible-hosting">Listings</Link>
+          <Link to="/communication-center">Communication Center</Link>
+          <Link to="/listings">Listings</Link>
+        </div>
       </div>
     );
   }
